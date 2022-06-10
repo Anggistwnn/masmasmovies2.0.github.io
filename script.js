1c5247bd4951f6680ac3bde35b923c494130978a
Anggistwnn <Anggistwnn@gmail.com>
2022-06-10 19:26:13 +0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 $.ajax({
-    url: 'http://www.omdbapi.com/?apikey=21cf67eb&s=breaking bad',
+    url: 'http://www.omdbapi.com/?apikey=21cf67eb&s=harry potter',
     success: result => {
-        const moviess = result.Search;
-        console.log(moviess);
-
+        const movies = result.Search;
+        console.log(movies);
+        let cards = '';
+        movies.forEach(m => {
+            cards += `<div class="col-md-4 my-5">
+                        <div class="card">
+                            <img src="${m.Poster}" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">${m.Title}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                                <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#movieDetailModal">Show Details</a>
+                            </div>
+                        </div>
+                    </div>`
+        });
+        $('.movies-container').html(cards);
     },
     error: (e) => {
         console.log(e.responseText);
