f3909bbe750f6289163cb243dbffa0446f5f0558
Anggistwnn <Anggistwnn@gmail.com>
2022-06-10 18:33:24 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $.ajax({
     url: 'http://www.omdbapi.com/?apikey=21cf67eb&s=breaking bad',
     success: result => {
-        console.log(result);
+        const movies = result.Search;
+        console.log(movies);
+
     },
     error: (e) => {
         console.log(e.responseText);
