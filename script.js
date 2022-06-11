f8bf0dcf00274483980655924c8b1f8185859db0
Anggistwnn <Anggistwnn@gmail.com>
2022-06-11 09:36:36 +0700

    update - Promise

--- script.js
+++ script.js
@@ -115,3 +115,26 @@
 //     .catch(response => console.log('NOT OK! : ' + response));
 
 // Contoh 2
+let ditepati = true;
+const janji2 = new Promise((resolve, reject) => {
+    if (ditepati) {
+        setTimeout(() => {
+            resolve('Ditepati setelah beberapa waktu!')
+        }, 2000);
+    } else {
+        setTimeout(() => {
+            resolve('Tidak ditepati setelah beberapa waktu!')
+        }, 2000);
+    }
+});
+
+console.log('mulai');
+janji2
+    .finally(() => console.log('selesai menunggu'))
+    .then(response => console.log('OK! : ' + response))
+    .catch(response => console.log('NOT OK! : ' + response));
+// console.log(janji2
+//     .then(() => {
+//         console.log(janji2);
+//     }));
+console.log('selesai');
