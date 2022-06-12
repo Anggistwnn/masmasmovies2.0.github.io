7fed2f05758bbc01d1e9d3467fd0c03bef8897a1
Anggistwnn <Anggistwnn@gmail.com>
2022-06-12 22:43:21 +0700

    update

--- script.js
+++ script.js
@@ -33,43 +33,105 @@
 // });
 
 // fetch
+// const searchButton = document.querySelector('.search-button');
+// searchButton.addEventListener('click', function () {
+
+//     const inputKeyword = document.querySelector('.input-keyword')
+//     fetch('http://www.omdbapi.com/?apikey=21cf67eb&s=' + inputKeyword.value)
+//         .then(response => response.json())
+//         .then(response => {
+//             const movies = response.Search;
+//             let cards = '';
+//             movies.forEach(m => cards += showCards(m));
+//             const moviesContainer = document.querySelector('.movies-container');
+//             moviesContainer.innerHTML = cards;
+
+
+//             // Ketika tombol detail di-klik
+//             const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+//             modalDetailButton.forEach(btn => {
+//                 btn.addEventListener('click', function () {
+//                     const imdbid = this.dataset.imdbid;
+//                     fetch('http://www.omdbapi.com/?apikey=21cf67eb&i=' + imdbid)
+//                         .then(response => response.json())
+//                         .then(m => {
+//                             const moviesDetail = showMovieDetail(m);
+//                             const modalBody = document.querySelector('.modal-body');
+//                             modalBody.innerHTML = moviesDetail;
+//                         });
+//                 });
+//             });
+
+//         });
+
+// });
+
+// fungsi pada button search
 const searchButton = document.querySelector('.search-button');
-searchButton.addEventListener('click', function () {
+searchButton.addEventListener('click', async function () {
+    try {
+        const inputKeyword = document.querySelector('.input-keyword');
+        const movies = await getMovies(inputKeyword.value);
+        // console.log(movies);
+        updateUI(movies);
+    } catch (err) {
+        alert(err);
+    }
+});
 
-    const inputKeyword = document.querySelector('.input-keyword')
-    fetch('http://www.omdbapi.com/?apikey=21cf67eb&s=' + inputKeyword.value)
+function getMovieDetail(imdbid) {
+    return fetch('http://www.omdbapi.com/?apikey=21cf67eb&i=' + imdbid)
         .then(response => response.json())
+        .then(m => m);
+};
+
+function updateUIDetail(m) {
+    const moviesDetail = showMovieDetail(m);
+    const modalBody = document.querySelector('.modal-body');
+    modalBody.innerHTML = moviesDetail;
+}
+
+
+// memanggil data film yang input oleh user
+function getMovies(keyword) {
+    return fetch('http://www.omdbapi.com/?apikey=21cf67eb&s=' + keyword)
         .then(response => {
-            const movies = response.Search;
-            let cards = '';
-            movies.forEach(m => cards += showCards(m));
-            const moviesContainer = document.querySelector('.movies-container');
-            moviesContainer.innerHTML = cards;
-
-
-            // Ketika tombol detail di-klik
-            const modalDetailButton = document.querySelectorAll('.modal-detail-button');
-            modalDetailButton.forEach(btn => {
-                btn.addEventListener('click', function () {
-                    const imdbid = this.dataset.imdbid;
-                    fetch('http://www.omdbapi.com/?apikey=21cf67eb&i=' + imdbid)
-                        .then(response => response.json())
-                        .then(m => {
-                            const moviesDetail = showMovieDetail(m);
-                            const modalBody = document.querySelector('.modal-body');
-                            modalBody.innerHTML = moviesDetail;
-                        });
-                });
-            });
-
-        });
-
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(response => {
+            if (response.Response === "False") {
+                throw new Error(response.Error);
+            }
+            // console.log(response);
+            return response.Search;
+        })
+};
+// Menampilkan film dicard
+function updateUI(movies) {
+    let cards = '';
+    movies.forEach(m => cards += showCards(m));
+    const moviesContainer = document.querySelector('.movies-container');
+    moviesContainer.innerHTML = cards;
+}
+// menampilkan detail film
+// event binding
+document.addEventListener('click', async function (e) {
+    if (e.target.classList.contains('modal-detail-button')) {
+        const imdbid = e.target.dataset.imdbid;
+        const movieDetail = await getMovieDetail(imdbid);
+        updateUIDetail(movieDetail);
+    }
 });
 
 
 
 
 
+
+// fungsi untuk menampilkan cardnya
 function showCards(m) {
     return `<div class="col-md-4 my-5">
                 <div class="card">
